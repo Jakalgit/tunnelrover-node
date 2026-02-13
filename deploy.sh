089fc7af28f08c16f26a945caaf7a9464ce7df57
misha <mistetsmit05@gmail.com>
2026-02-13 18:55:26 +0300

    Added iptables in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,42 @@
+#!/bin/bash
+
+set -e
+
 ROVER_NODE_HOST="web-node-1.tunnelrover.com"
 
-sudo apt update -y && sudo apt install certbot -y
+sudo iptables -F
+sudo iptables -X
 
-sudo certbot certonly --standalone -d $ROVER_NODE_HOST
+sudo apt update -y && sudo apt install certbot iptables-persistent -y
+
+sudo certbot certonly \
+  --standalone \
+  -d "$ROVER_NODE_HOST" \
+  --non-interactive \
+  --agree-tos \
+  --email info@tunnelrover.com \
+  --no-eff-email
 
 mkdir -p ./nginx-certs
 cp /etc/letsencrypt/live/$ROVER_NODE_HOST/fullchain.pem ./nginx-certs/
 cp /etc/letsencrypt/live/$ROVER_NODE_HOST/privkey.pem   ./nginx-certs/
 chmod 644 ./nginx-certs/*.pem
+
+sudo iptables -P INPUT DROP
+sudo iptables -P FORWARD DROP
+sudo iptables -P OUTPUT ACCEPT
+
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+sudo iptables -A FORWARD -p tcp --dport 8388:8488 -j ACCEPT
+sudo iptables -A FORWARD -p udp --dport 8388:8488 -j ACCEPT
+
+sudo netfilter-persistent save
 
 docker compose up -d
 
