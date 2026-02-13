052922768130ac3a47f369dc30595c47ecaa9989
misha <mistetsmit05@gmail.com>
2026-02-13 18:36:37 +0300

    Added deploy.sh

--- src/shadowsocks/services/shadowsocks.service.ts
+++ src/shadowsocks/services/shadowsocks.service.ts
@@ -1,18 +1,12 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import * as dgram from 'node:dgram';
 import { AddUsersDto } from '../dto/add-users.dto';
 import { RemoveUsersDto } from '../dto/remove-users.dto';
 
 @Injectable()
 export class ShadowsocksService {
-  private readonly host: string;
-  private readonly port: number;
-
-  constructor(private readonly configService: ConfigService) {
-    this.host = configService.get<string>('CONTAINER_HOST');
-    this.port = Number(configService.get<string>('CONTAINER_PORT') || 6100);
-  }
+  private readonly host: string = 'ss-rust';
+  private readonly port: number = 6100;
 
   async addUsers(dto: AddUsersDto) {
     const client = dgram.createSocket('udp4');
@@ -96,17 +90,5 @@ export class ShadowsocksService {
     } catch {
       throw new BadRequestException('Error parsing list of users');
     }
-  }
-
-  async sendCommand(command: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      const client = dgram.createSocket('udp4');
-
-      client.send(Buffer.from(command), this.port, this.host, (err) => {
-        client.close();
-        if (err) reject(err);
-        else resolve();
-      });
-    });
   }
 }
