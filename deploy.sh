c51c27e5c1cfacf95a4be61ee43d46a5eb4c2969
misha <mistetsmit05@gmail.com>
2026-02-13 19:42:26 +0300

    Update iptables in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,6 @@ set -e
 
 ROVER_NODE_HOST="web-node-1.tunnelrover.com"
 
-sudo iptables -F
-sudo iptables -X
-
 sudo apt update -y && sudo apt install certbot iptables-persistent -y
 
 sudo certbot certonly \
@@ -22,14 +19,15 @@ cp /etc/letsencrypt/live/$ROVER_NODE_HOST/fullchain.pem ./nginx-certs/
 cp /etc/letsencrypt/live/$ROVER_NODE_HOST/privkey.pem   ./nginx-certs/
 chmod 644 ./nginx-certs/*.pem
 
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
 sudo iptables -P INPUT DROP
 sudo iptables -P FORWARD DROP
 sudo iptables -P OUTPUT ACCEPT
 
 sudo iptables -A INPUT -i lo -j ACCEPT
-sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
