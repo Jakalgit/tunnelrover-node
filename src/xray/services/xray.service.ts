3d29bb4899c9c2190afe9eee6bf2d01ab3fc727c
misha <mistetsmit05@gmail.com>
2026-04-05 01:12:53 +0300

    Fixed bugs

--- src/xray/services/xray.service.ts
+++ src/xray/services/xray.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { XtlsApi } from '@remnawave/xtls-sdk';
-import { UpdateUsersDto } from '../dto/update-users.dto';
 
 @Injectable()
 export class XrayService {
   private api!: XtlsApi;
+  private readonly TAGS = ['vless-tcp', 'vless-ws'];
 
   constructor(private readonly configService: ConfigService) {
     const XRAY_HOST = configService.get<string>('XRAY_HOST');
@@ -15,32 +15,63 @@ export class XrayService {
     });
   }
 
-  async addUser(dto: UpdateUsersDto) {
-    const promises: Promise<any>[] = dto.uuids.map((uuid) =>
-      this.api.handler.addVlessUser({
-        level: 0,
-        uuid,
-        tag: dto.tag,
-        username: uuid,
-        flow: 'xtls-rprx-vision',
-      }),
-    );
+  async addUser(uuids: string[]) {
+    const promises: Promise<any>[] = uuids
+      .map((uuid) => {
+        return this.TAGS.map((tag) =>
+          this.api.handler.addVlessUser({
+            level: 0,
+            uuid,
+            tag,
+            username: uuid,
+            flow: tag === 'vless-tcp' ? 'xtls-rprx-vision' : '',
+          }),
+        );
+      })
+      .flat();
 
     return await Promise.all(promises);
   }
 
-  async removeUser(dto: UpdateUsersDto) {
-    const promises = dto.uuids.map((el) =>
-      this.api.handler.removeUser(dto.tag, el),
-    );
+  async removeUser(uuids: string[]) {
+    const promises = uuids
+      .map((el) => {
+        return this.TAGS.map((tag) => this.api.handler.removeUser(tag, el));
+      })
+      .flat();
     return await Promise.all(promises);
   }
 
-  getInboundUsers(tag: string) {
-    return this.api.handler.getInboundUsers(tag);
+  async getInboundUsers() {
+    const users: Set<string> = new Set();
+    for (const tag of this.TAGS) {
+      const response = await this.api.handler.getInboundUsers(tag);
+      if (response.isOk) {
+        response.data.users.forEach((user) => {
+          users.add(user.username);
+        });
+      }
+    }
+
+    return {
+      uuids: users.values(),
+    };
   }
 
-  getInboundUsersCount(tag: string) {
-    return this.api.handler.getInboundUsersCount(tag);
+  async getInboundUsersCount() {
+    let maxUsers = 1000000000;
+
+    for (const tag of this.TAGS) {
+      const response = await this.api.handler.getInboundUsersCount(tag);
+
+      if (response.isOk) {
+        if (maxUsers === 1000000000) {
+          maxUsers = 0;
+        }
+        maxUsers = Math.max(maxUsers, response.data);
+      }
+    }
+
+    return maxUsers;
   }
 }
